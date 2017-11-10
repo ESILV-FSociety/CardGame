91d637c45c9e5012649920bf9d6b3b799661ce34
Maxime Rebillard <rebillardmaxime@gmail.com>
2017-11-10 17:04:34 +0100

    Some Changes on Class: - Dryad - Elf

--- src/main/java/fr/esilv/fsociety/cardgame/api/races/Dryad.java
+++ src/main/java/fr/esilv/fsociety/cardgame/api/races/Dryad.java
@@ -6,14 +6,18 @@ import fr.esilv.fsociety.cardgame.api.Game;
 public class Dryad extends Card {
 
 	public Dryad() {
-		super("Dryad", "dryad.jpg");
-		
+		super("Dryad","CardGame\\src\\main\\java\\resources\\fr.esilv.fsociety.cardgame\\cards\\dryad.png");
+	}
+
+	public void activatePower(){
+		// stole a card in front of your opponent and add it in front of you without activating its power.
+
 	}
 
 	@Override
 	public void activate(Game theGame) {
-		
-		
+		//TODO
+
 	}
 
 }
--- src/main/java/fr/esilv/fsociety/cardgame/api/races/Elf.java
+++ src/main/java/fr/esilv/fsociety/cardgame/api/races/Elf.java
@@ -6,14 +6,18 @@ import fr.esilv.fsociety.cardgame.api.Game;
 public class Elf extends Card {
 
 	public Elf() {
-		super("Elf", "elf.jpg");
-		
+		super("Elf","CardGame\\src\\main\\java\\resources\\fr.esilv.fsociety.cardgame\\cards\\elf.png");
+	}
+
+	public void activatePower(){
+		// copy and use the power of one of the card in front of you
+
 	}
 
 	@Override
 	public void activate(Game theGame) {
-		
-		
+		//TODO
+
 	}
 
 }
