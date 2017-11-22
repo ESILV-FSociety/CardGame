88f87e95688f669b7083cc88c2a17a3d621aaa6b
jlabarre91 <jlabarre91130@gmail.com>
2017-11-22 17:35:34 +0100

    Class Troll + test troll + depende pour cucumber sur le pom

--- src/main/java/fr/esilv/fsociety/cardgame/api/races/Troll.java
+++ src/main/java/fr/esilv/fsociety/cardgame/api/races/Troll.java
@@ -1,6 +1,10 @@
 package fr.esilv.fsociety.cardgame.api.races;
+import fr.esilv.fsociety.cardgame.api.Board;
 import fr.esilv.fsociety.cardgame.api.Card;
 import fr.esilv.fsociety.cardgame.api.Game;
+import fr.esilv.fsociety.cardgame.api.Player;
+
+import java.util.ArrayList;
 
 public class Troll extends Card {
 
@@ -8,9 +12,14 @@ public class Troll extends Card {
 		super("Troll", "images/troll.png");
 	}
 
-	public void activatePower(){
-		// swap the cards in front of you with the cards in front of your opponent
+	public void activatePower(Player j1, Player j2){
+		ArrayList<Card> tempKingdom  =  new ArrayList<>();
+		System.out.println("Troll activated");
+		tempKingdom = j1.getBoard().getKingdom();
+		j1.getBoard().setKingdom(j2.getBoard().getKingdom());
+		j2.getBoard().setKingdom(tempKingdom);
 
+		// swap the cards in front of you with the cards in front of your opponent
 	}
 
 	@Override
