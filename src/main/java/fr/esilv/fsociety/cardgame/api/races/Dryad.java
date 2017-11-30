9f1025b642b3a745a8b14f55eabd08ae1167a240
manasa prakash <manasa.prakashv@gmail.com>
2017-11-30 01:29:35 +0100

    Dryard's activatePower defined.

--- src/main/java/fr/esilv/fsociety/cardgame/api/races/Dryad.java
+++ src/main/java/fr/esilv/fsociety/cardgame/api/races/Dryad.java
@@ -2,6 +2,9 @@ package fr.esilv.fsociety.cardgame.api.races;
 
 import fr.esilv.fsociety.cardgame.api.Card;
 import fr.esilv.fsociety.cardgame.api.Game;
+
+import java.util.Random;
+
 public class Dryad extends Card {
 
 
@@ -12,6 +15,15 @@ public class Dryad extends Card {
 	public void activatePower(Game game){
 		// stole a card in front of your opponent and add it in front of you without activating its power.
 		//TODO
+
+        Random random = new Random();
+
+        // We need to copy a card that we have in our kingdom (<==> an index where the value is not equal to 0)
+        int n [] = game.getOpponentPlayer().getBoard().getKingdom();
+
+        int rand= random.nextInt(n.length);
+
+        game.getCurrentPlayer().getBoard().getKingdom()[rand]+= 1;
 	}
 
 
